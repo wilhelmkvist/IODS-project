cf21dbdd1d57d72ac15b86e1af2c2198712fc6d9
Wilhelm <wilhelm@MBP13-2-2021.local>
2021-12-03 22:35:49 +0200

    Completed the data wrangling part

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,10 @@
 # Wilhelm Kvist 29 Nov 2021
-# This script is created as part of exercise 4 for the IODS coursein the Fall of 2021.
+# This script is created as part of exercises 4 and 5 for the IODS course in the Fall of 2021.
+# The human dataset contains country-specific indicators relating to human development. The Human Development Index (HDI) is calculated by the UNDP and is a summary measure of average achievement in key dimensions of human development. Variables include data on Gross National Income per capita, life expectancy, expected years of education and female represenatation in parliament, secondary education and the labour force.
+# A description of variables can be found here: https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Open-Data-Science/master/datasets/human_meta.txt
+# Original data: http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
+# Technical notes on calculations: http://hdr.undp.org/sites/default/files/hdr2015_technical_notes.pdf.
+# Source: United Nations Development Programme.
 
 # Reading “Human development” and “Gender inequality”
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -19,7 +24,7 @@ summary(gii)
 
 # Renaming columns
 colnames(hd)[3] <- "HDI"
-colnames(hd)[4] <- "Lifeexp"
+colnames(hd)[4] <- "LifeExp"
 colnames(hd)[5] <- "EducExp"
 colnames(hd)[6] <- "EducMean"
 colnames(hd)[7] <- "GNI"
@@ -51,7 +56,42 @@ setwd("~/Documents/IODS-project")
 write.csv(human, file = "./data/human.csv", row.names = F)
 
 # Reading the data again
-import_data <- read.csv("./data/human.csv", stringsAsFactors = F)
+human <- read.csv("./data/human.csv", stringsAsFactors = F)
 
 # Confirming the structure of the imported dataset
-str(import_data)
+str(human)
+
+#--------Wrangling for exercise 5--------
+
+# Mutate the data: transform the Gross National Income (GNI) variable to numeric
+library(stringr)
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+# Exclude unneeded variables: keep only select columns
+keep <- c("Country", "EduRatio", "LabRatio", "EducExp", "LifeExp", "GNI", "Matmort", "AdolBirthRt", "ParlRep")
+human <- select(human, one_of(keep))
+
+# Remove all rows with missing values
+human <- filter(human, complete.cases(human) == TRUE)
+
+# Remove the observations which relate to regions instead of countries. (Rows 1 to 155 seem to be related to countries.)
+human <- human[1:155, ]
+
+# Define the row names of the data by the country names and remove the country name column from the data.
+rownames(human) <- human$Country
+human <- select(human, -Country)
+
+# The data should now have 155 observations and 8 variables. Confirming the dimensions.
+dim(human)
+
+# Save the human data in your data folder including the row names. You can overwrite your old ‘human’ data.
+write.csv(human, file = "./data/human.csv", row.names = T)
+
+# Reading the data again
+human <- read.csv("./data/human.csv", stringsAsFactors = F, row.names = 1)
+
+# Confirming the structure of the imported dataset
+str(human)
+head(human)
+
+# I note that I had to include the 'row.names = 1' argument in the read.csv command in order to get the row names proper order when importing the data back. The data seems to look fine.
